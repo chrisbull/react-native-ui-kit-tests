896ff8ab1a0130a8a45fc70499cd9173a29b9a2b
Chris Bull <chris@chrisbull.com>
2018-08-18 14:38:58 -0700

    react-native link lottie-ios lottie

--- android/app/src/main/java/com/uikitstests/MainApplication.java
+++ android/app/src/main/java/com/uikitstests/MainApplication.java
@@ -3,6 +3,8 @@ package com.uikitstests;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnative.photoview.PhotoViewPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.github.xinthink.rnmk.ReactMaterialKitPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new PhotoViewPackage(),
+            new LinearGradientPackage(),
             new VectorIconsPackage(),
             new RNSpinkitPackage(),
             new ReactMaterialKitPackage(),
