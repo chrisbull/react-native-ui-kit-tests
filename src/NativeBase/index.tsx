205939812e004ec2f095906871a3ee04afcdb25b
Chris Bull <chris@chrisbull.com>
2018-08-18 03:09:22 -0700

    update native-base

--- src/NativeBase/index.tsx
+++ src/NativeBase/index.tsx
@@ -1,14 +1,16 @@
-import { StyleProvider, Container, Content, Text } from "native-base";
+import React, { Component } from "react";
+import { Container, Content, Text, StyleProvider } from "native-base";
 
-import getTheme from "./native-base-theme/components/index.js";
+import getTheme from "./theme/components";
+import material from "./theme/variables/material";
 
-export default class NativeBase extends Components {
+export default class ThemeExample extends Component {
   render() {
     return (
-      <StyleProvider style={getTheme()}>
+      <StyleProvider style={getTheme(material)}>
         <Container>
           <Content>
-            <Text>Hello World</Text>
+            <Text>I have changed the text color.</Text>
           </Content>
         </Container>
       </StyleProvider>
